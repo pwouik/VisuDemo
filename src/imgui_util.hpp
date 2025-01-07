b2b8dcb71f14fdee9bbe3ea38adf2bb8d98bef5d
Sixelayo <66423373+Sixelayo@users.noreply.github.com>
2025-01-07 16:44:10 +0100

    the attroctor is not attracting

--- src/imgui_util.hpp
+++ src/imgui_util.hpp
@@ -20,6 +20,14 @@ static void HelpMarker(const char* desc){
     }
 }
 
+static void ShowMatrix4(const char* desc, const glm::mat4& mat) {
+    ImGui::Text(desc);
+    for (int row = 0; row < 4; ++row) {
+        ImGui::Text("%.2f  %.2f  %.2f  %.2f", 
+                    mat[row][0], mat[row][1], mat[row][2], mat[row][3]);
+    }
+}
+
 
 static void initIMGUI(GLFWwindow* window){
     // Setup Dear ImGui context
