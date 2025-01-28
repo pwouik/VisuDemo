a75c3b1f9981222346f379f8351f7df323241e2f
pwouik <pwouick@gmail.com>
2025-01-28 12:34:03 +0100

    temp

--- src/app.cpp
+++ src/app.cpp
@@ -9,6 +9,8 @@
 #include <glm/fwd.hpp>
 #include <glm/trigonometric.hpp>
 #include <glm/gtx/rotate_vector.hpp>
+#include "opengl_util.h"
+
 
 #include <iostream>
 #include <memory>
@@ -22,7 +24,6 @@
 #include "glm/ext.hpp"
 #include <algorithm> //for std::fill
 #include <mutex>
-#include <vector>
 
 // #include <thread> //this therad
 // #include <chrono> //sleep
@@ -37,56 +38,6 @@ using namespace std;
 
 #define NBPTS 20000000
 
-
-std::string readShaderSource(const std::string& shaderFile)
-{
-    std::ifstream file(shaderFile);
-    if (!file.is_open())
-    {
-        std::cerr << "Error could not open file " << shaderFile << std::endl;
-        return "";
-    }
-    std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-    file.close();
-    return content;
-}
-
-GLuint loadshader(const char* file,GLuint type)
-{
-    const std::string shaderSource = readShaderSource(file);
-    const GLchar* source = shaderSource.c_str();
-    GLuint shader = glCreateShader(type);
-    glShaderSource(shader, 1, &source, nullptr);
-    glCompileShader(shader);
-    GLint compiled;
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
-    if (!compiled) {
-        printf("%s failed to compile:\n", file);
-        GLint logSize;
-        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logSize);
-        char* logMsg = new char[logSize];
-        glGetShaderInfoLog(shader, logSize, nullptr, logMsg);
-        printf("%s\n", logMsg);
-        delete[] logMsg;
-        exit(EXIT_FAILURE);
-    }
-    return shader;
-}
-
-void linkProgram(GLuint program){
-    GLint linked;
-    glGetProgramiv(program, GL_LINK_STATUS, &linked);
-    if (!linked) {
-        printf("Shader program failed to link:\n");
-        GLint logSize;
-        glGetProgramiv(program, GL_INFO_LOG_LENGTH, &logSize);
-        char* logMsg = new char[logSize];
-        glGetProgramInfoLog(program, logSize, nullptr, logMsg);
-        printf("%s\n", logMsg);
-        delete[] logMsg;
-        exit(EXIT_FAILURE);
-    }
-}
 
 
 namespace gbl{
