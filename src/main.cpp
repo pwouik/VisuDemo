4bb55a2bc711cb65a86f11e616b4fe562695cd8e
Sixelayo <66423373+Sixelayo@users.noreply.github.com>
2025-01-30 12:06:29 +0100

    save random seed at launch

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,19 @@
 #include <cstdlib>
 #include <cstdio>
 #include <cmath>
+#include <iostream>
+
 #define GLAD_GL_IMPLEMENTATION // Necessary for headeronly version.
 
 
 int main(void) {
+    //save seed so if we ever see smth rly cool we can reset it
+    unsigned int seed = static_cast<unsigned int>(std::time(nullptr));
+    std::srand(seed);
+    std::cout << "seed initialized : " << seed << std::endl;
+
     App app(1600,900);
     app.setupLeapMotion();
-    std::srand(static_cast<unsigned int>(std::time(nullptr)));
     app.run();
     return 0;
 }
